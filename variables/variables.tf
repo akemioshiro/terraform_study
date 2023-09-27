b82ab4fc451fff64c9f047745a27a14b8a644686
Patricia Oshiro <akemioshiro@hotmail.com>
2023-09-26 22:50:32 -0300

    feat add variables teste

---
+++ variables/variables.tf
@@ -0,0 +1,15 @@
+variable filename1 {
+  type        = string
+  default     = "file2.txt"
+  description = "description"
+}
+
+variable content1 {
+  type        = string
+  default     = "Variaveis no terraform"
+  description = "description"
+}
+
+# terraform init
+# terraform plan
+# terraform apply
