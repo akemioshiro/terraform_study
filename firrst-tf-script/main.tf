b8ea760e7de5cb37dd7a638cdac724285bf9d1d0
Patricia Oshiro <akemioshiro@hotmail.com>
2023-09-26 22:20:39 -0300

    feat: add samples tests

---
+++ firrst-tf-script/main.tf
@@ -0,0 +1,7 @@
+resource local_file sample_res {
+  filename = "sample.txt"
+  content = "I love Terraform"
+}
+# terraform init
+# terraform plan
+# terraform apply
---
+++ local_arg/main.tf
@@ -0,0 +1,8 @@
+resource local_file sample_res {
+  filename = "sample_args.txt"
+  sensitive_content = "I love Terraform"
+  file_permission = "0700"
+}
+# terraform init
+# terraform plan
+# terraform apply
